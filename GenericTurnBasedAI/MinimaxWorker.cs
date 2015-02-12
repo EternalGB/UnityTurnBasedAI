4660b13f38bc27d1cb8025515f5076fa8db8d746
RaphByrne <raph.byrne@gmail.com>
2015-02-12 18:25:14 +0800

    finished up comments

--- GenericTurnBasedAI/MinimaxWorker.cs
+++ GenericTurnBasedAI/MinimaxWorker.cs
@@ -5,22 +5,42 @@ using UnityEngine;
 namespace UniversalTurnBasedAI
 {
 
-
+	/// <summary>
+	/// Holds all of the initialising information required for a Minimax search so it
+	/// can more easily be passed to a ThreadPool
+	/// </summary>
 	public class MinimaxWorker
 	{
 
 		GameState rootState;
+		/// <summary>
+		/// The first turn to use in the search
+		/// this is the turn retrieved for returning
+		/// </summary>
 		public Turn firstTurn;
 		int maxDepth;
 		bool ourTurn;
 		Evaluator eval;
 		EventWaitHandle waitHandle;
+		/// <summary>
+		/// The value of <paramref name="firstTurn"/>
+		/// </summary>
+		/// <value>The value.</value>
 		public float Value
 		{
 			get; private set;
 		}
 		bool stopped;
 
+		/// <summary>
+		/// Initializes a new instance of the <see cref="UniversalTurnBasedAI.MinimaxWorker"/> class.
+		/// </summary>
+		/// <param name="rootState">The starting state</param>
+		/// <param name="firstTurn">The turn to apply to the starting state to generate this worker's branch</param>
+		/// <param name="eval">The Evaluator</param>
+		/// <param name="maxDepth">Max depth.</param>
+		/// <param name="ourTurn">Whether or not it is the searching player's turn</param>
+		/// <param name="waitHandle">Signals the ThreadPool that the search is complete</param>
 		public MinimaxWorker (GameState rootState, Turn firstTurn, Evaluator eval, int maxDepth, bool ourTurn, EventWaitHandle waitHandle)
 		{
 			this.rootState = rootState;
